46799d9019046baa3710ef55656316344ef27542
Hari <harikrishkk@gmail.com>
2023-06-19 18:47:26 -0400

    commit 6: add meta reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
+import { ActionReducer, MetaReducer, StoreModule } from '@ngrx/store';
 import { CounterComponent } from './components/counter/counter.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { booksReducer } from './store/books/books.reducer';
@@ -10,6 +10,17 @@ import { BookCollectionComponent } from './components/book-collection/book-colle
 import { HttpClientModule } from '@angular/common/http';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+// Higher order function
+export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
+  return function (state, action) {
+    console.log('state', state);
+    console.log('action', action);
+
+    return reducer(state, action);
+  };
+}
+
+export const metaReducers: MetaReducer<any>[] = [debug];
 
 @NgModule({
   declarations: [
@@ -24,7 +35,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     StoreModule.forRoot({
       books: booksReducer,
       collection: collectionReducer
-    }, {}),
+    }, { metaReducers }),
     StoreDevtoolsModule.instrument({
       maxAge: 25
     }),
